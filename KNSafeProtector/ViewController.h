2903f61e1a85c1839a66e72aa3254f347457507b
zhangkn <929118967@qq.com>
2019-03-11 14:46:27 +0800

    Initial Commit

---
+++ KNSafeProtector/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KNSafeProtector
+//
+//  Created by mac on 2019/3/11.
+//  Copyright © 2019 kunnan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ KNSafeProtector/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  KNSafeProtector
+//
+//  Created by mac on 2019/3/11.
+//  Copyright © 2019 kunnan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
